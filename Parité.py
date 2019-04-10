23d9d6c68ee673a80d5982a166e9937c0c707f49
michelges <49408842+michelges@users.noreply.github.com>
2019-04-10 14:43:37 +0100

    Delete Parité.py

--- Parité.py
+++
@@ -1,9 +0,0 @@
-#Programme vérifiant la parité d'un nombre
-
-
-a = int(input( "Veuillez saisir un nombre s.v.p \n")) 
-if a%2 == 0:        #Instruction de la Condition alternative
-    print("La valeur entrée est paire \n")
-else:
-    print("La valeur entrée est impaire \n")
-    
