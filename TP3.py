b194105ef0b9defaef385f823a952ef67148749c
michelges <49408842+michelges@users.noreply.github.com>
2019-04-10 14:42:17 +0100

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -2,9 +2,9 @@
 
 
 n = int(input( "Veuillez saisir un nombre de votre choix \n")) 
-if n<=10:        #Instruction de la Condition alternative
+if n<=9:        #Instruction de la Condition alternative
     print("Condition vérifiée \n")
-    print("Bravo ! Réponse correcte \n")
+    print("Bravo ! Vous avez réussi \n")
 else:
     print("Condition non vérifiée \n")
     print("Dommage ! Mauvaise réponse \n")
