73ad9d9e92c6e2d0d4797e5c52aae21bbf95cdff
michelges <49408842+michelges@users.noreply.github.com>
2019-04-10 14:37:49 +0100

    Add files via upload

---
+++ TP3.py
@@ -0,0 +1,11 @@
+#Programme 
+
+
+n = int(input( "Veuillez saisir un nombre de votre choix \n")) 
+if n<=10:        #Instruction de la Condition alternative
+    print("Condition vérifiée \n")
+    print("Bravo ! Réponse correcte \n")
+else:
+    print("Condition non vérifiée \n")
+    print("Dommage ! Mauvaise réponse \n")
+    
