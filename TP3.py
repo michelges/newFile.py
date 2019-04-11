e9ec6b2382cbc7dd7ea10cf86dd09928ddac1ae2
michelges <49408842+michelges@users.noreply.github.com>
2019-04-11 11:47:22 +0100

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -5,11 +5,13 @@ nbre_alea = random.randint(0,9)
 
 rep_user = None
 
-while rep_user != nbre_alea:
-    rep_user = int(input("veuillez saisir une valeur de votre choix svp comprise entre 0 et 9 \n"))
-    if rep_user < nbre_alea:
-        print("Dommage ! Réessayez à nouveau, \n") 
+for i in range(3):
+
+    if rep_user != nbre_alea:
+        rep_user = int(input("veuillez saisir une valeur de votre choix svp comprise entre 0 et 9 \n"))
+    if rep_user > nbre_alea:
+        print("Réessayez à nouveau, \n") 
     elif rep_user > nbre_alea:
-        print("Vous y êtes presque \n")
+        print("Encore une fois \n")
     else:
         print("Bravo !!! Vous avez trouvé. Félicitation !")
