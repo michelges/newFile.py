bba416438b269d0ac5939346d9905188918259bc
michelges <49408842+michelges@users.noreply.github.com>
2019-04-11 10:42:36 +0100

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -1,11 +1,15 @@
-#Programme 
+#Programme nombres aléatoires
 
+import random
+nbre_alea = random.randint(0,9)
 
-n = int(input( "Veuillez saisir un nombre de votre choix \n")) 
-if n<=9:        #Instruction de la Condition alternative
-    print("Condition vérifiée \n")
-    print("Bravo ! Vous avez réussi \n")
-else:
-    print("Condition non vérifiée \n")
-    print("Dommage ! Mauvaise réponse \n")
-    
+rep_user = None
+
+while rep_user != nbre_alea:
+    rep_user = int(input("veuillez saisir une valeur de votre choix svp comprise entre 0 et 9 \n"))
+    if rep_user < nbre_alea:
+        print("Dommage ! Réessayez à nouveau, \n") 
+    elif rep_user > nbre_alea:
+        print("Vous y êtes presque \n")
+    else:
+        print("Bravo !!! Vous avez trouvé. Félicitation !")
