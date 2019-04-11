aca26339dac45462944bdc91b0e3b92bc80285d2
michelges <49408842+michelges@users.noreply.github.com>
2019-04-11 11:53:51 +0100

    Update newFile.py

--- newFile.py
+++ newFile.py
@@ -1,10 +1,10 @@
-#Table de multiplication
-i = 1
-while i <= 20:
-    nb = 1
-    while nb <= 20:
-        print(i*nb, end = " ")
-        nb = nb + 1
-    print("")
-    i = i + 1
-    
+
+#declaration des variables
+a=float(input("entrer un nombre \n")) #float représente la famille des réels
+b=float(input("entrer un nombre \n"))
+
+
+moy=float((a+b)/2) #ici on applique la somme des deux valeurs, puis on divise par 2
+
+print("la moyenne des deux variables est :") #instruction permettant à la machine d'afficher le résultat
+print(moy) #Voici le résultat final affiché par la machine
